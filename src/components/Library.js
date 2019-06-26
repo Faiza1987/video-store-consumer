5a7d7131d492271468648ec85445cf50e850f882
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-06-25 22:20:46 -0700

    Moved componentDidMount from App to Movies

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,31 +1,63 @@
 import React, { Component } from 'react';
 import './Library.css';
+import axios from 'axios';
 
+class Library extends Component {
+  constructor(props){
+    super(props);
 
-const Library = (props) => {
+    this.state = {
+      displayMovieList: false
+    }
+  }
   
-  const mappedMovies = props.allMovies.map((movie, i) => {
+  componentDidMount() {
+    axios.get('http://localhost:3000/movies')
+      .then((response) => {
+
+        console.log('movie response.data', response.data);
+
+        this.props.setAllMoviesCallback(response.data);
+      })
+      .catch((error) => {
+        this.setState({
+          errorMessage: error.message
+        })
+      })
+  }
+
+  toggleDisplayMovieList = (props) => {
+    console.log("I'm in toggleAllMovies Function!");
+    this.setState({
+      displayMovieList: !this.state.displayMovieList,
+    });
+  }
+  
+  render() {
+    const {displayMovieList} = this.state;
+    const {movies, onSelectMovie} = this.props;
+
+    const mappedMovies = movies.map((movie, i) => {
+      return(
+        <button key={i} onClick={onSelectMovie(movie.id)} className="movie-button"> {movie.title} </button>
+      );
+    });
     return(
-      <button key={i} onClick={props.onSelectMovie(movie.id)} className="movie-button"> {movie.title} </button>
+      <div>
+        <section>
+          <button type='button' onClick={this.toggleDisplayMovieList}>
+            See All Movies
+          </button>
+        </section>
+      
+        {displayMovieList && <section>
+          {mappedMovies}
+        </section>}
+  
+      </div>
+      
     );
-  });
-
-
-  return(
-    <div>
-      <section>
-        <button type='button' onClick={props.toggleDisplayStatusCallback}>
-          See All Movies
-        </button>
-      </section>
-    
-      {props.displayStatus && <section>
-        {mappedMovies}
-      </section>}
-
-    </div>
-    
-  );
+  }
 }
 
 export default Library;
