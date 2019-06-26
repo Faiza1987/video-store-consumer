29414ec571c47a2dcbd50db908ca91cddc0c7c93
Heather Izumi <hkizumi@gmail.com>
2019-06-26 13:35:51 -0700

    selected customer and movie persist and are viewable throughout the app

--- src/components/Rental.js
+++ src/components/Rental.js
@@ -7,16 +7,36 @@ class Rental extends Component {
     constructor(props) {
         super(props);
     }
-    
+
+    // componentDidMount(props) {
+    //     console.log('I mounted! Here are my props: ', props);
+    // };
 
 
-    componentDidMount(props) {
-        console.log('I mounted! Here are my props: ', props);
-    };
+
+      addDays = (date, days) => {
+        let result = new Date(date);
+        result.setDate(result.getDate() + days);
+        return result;
+      }
+    //   from https://stackoverflow.com/questions/563406/add-days-to-javascript-date
+
+
+    // rentMovie = (movie, customer) => {
+
+
+    // }
 
     render() {
+        const { rentalCustomer, rentalMovie } = this.props;
+
+        console.log(this.addDays(Date.now(), 7));
+
         return (
-            <div>I'm in rental!!!!!!</div>
+            <div>
+                {rentalMovie && <div>{rentalMovie.title}</div>}
+                {rentalCustomer && <div>{rentalCustomer.name}</div>}
+            </div>
         )
     }
 }
